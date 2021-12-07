6ba4eaf808e524bad30d532df955f9c30a418896
SilverIZ <silverzulkarnaen@gmail.com>
2021-12-07 12:57:00 +0700

    ISK (HP, DL) add detail page UI and optimize home UI
    not connected to redux yet

--- src/pages/detail.js
+++ src/pages/detail.js
@@ -1,49 +1,59 @@
 import { React, useState, useEffect } from "react";
 import axios from "axios";
 import "../styles/pages/detail.scss";
-import {
-  Button,
-  Desc,
-  Image,
-  Title,
-  Category,
-  Price,
-} from "../components/itemcard/index.js";
+import Spinner from "../components/loading/Spinner";
+import { useHistory } from "react-router";
+import { Image, Title, Category, Price } from "../components/itemcard/index.js";
 
 function Detail(props) {
   var id = props.match.params.id;
+  const token = useState(false);
   //test
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [product, setProduct] = useState([]);
+  const history = useHistory();
+  const [currentStock, setCurrentStock] = useState(100);
 
+  const addToCart = () => {
+    if (token === true) {
+      console.log("kirim ke halaman chart");
+    } else {
+      history.push("/login");
+    }
+  };
   useEffect(() => {
-    fetch("https://fakestoreapi.com/products/" + id)
-      .then((res) => res.json())
-      .then(
-        (data) => {
-          console.log(data);
-          setProduct(data);
-          setIsLoaded(true);
-        },
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      );
-  }, []);
+    axios
+      .get("https://fakestoreapi.com/products/" + id)
+      .then((response) => {
+        setProduct(response.data);
+        setIsLoaded(true);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        setIsLoaded(true);
+        setError(error);
+        console.log(error);
+      });
+  }, [id]);
+
   if (error) {
-    return <div>Error: {error.message}</div>;
+    return (
+      <div className="errormessage">
+        <h1>
+          <strong>404 |</strong> Not Found
+        </h1>
+        Keterangan : {error.message}
+      </div>
+    );
   }
   if (!isLoaded) {
-    return <div>Loading...</div>;
+    return <Spinner />;
   }
 
   if (product) {
     return (
       <>
-        <div> {id}</div>
-
         <section className="product">
           <div className="Product-card">
             <div className="Product-image">
@@ -62,10 +72,19 @@ function Detail(props) {
                   <Price price={product.price} />
                 </div>
               </div>
-              <button className="buy--btn">ADD TO CART</button>
-              {/* <Button theme={"white"} click={() => props.add(props.save)}>
-                Add to Chart
-              </Button> */}
+              <div className="Product-quantity">
+                <input
+                  type="number"
+                  value={currentStock}
+                  min={0}
+                  onChange={(v) => setCurrentStock(v.target.value)}
+                  style={{ width: 50 }}
+                />
+                <button type="button" className="buy--btn" onClick={addToCart}>
+                  ADD TO CART
+                </button>
+              </div>
+
               <div className="Product-description">
                 <h1>Product Detail</h1>
                 <div className="description">{product.description}</div>
